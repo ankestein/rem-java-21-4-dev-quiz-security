6cc7a80fe6f08d93382e0f5c49bf23a0b98e1094
Anke <a.stein@posteo.de>
2021-10-20 21:10:28 +0200

    Add LoginController (without JWT yet) and add handler for BadCredentials to ControllerAdvisor

--- backend/src/main/java/de/neuefische/devquiz/controller/exception/ControllerAdvisor.java
+++ backend/src/main/java/de/neuefische/devquiz/controller/exception/ControllerAdvisor.java
@@ -4,6 +4,7 @@ package de.neuefische.devquiz.controller.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,6 +31,15 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         ApiError apiError = new ApiError("Given input is not processable", ex.getMessage());
 
         return new ResponseEntity<>(apiError, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex) {
+        log.error("Username and/or password are not valid!", ex);
+
+        ApiError apiError = new ApiError("Username and/or password are not valid!", ex.getMessage());
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Throwable.class)
