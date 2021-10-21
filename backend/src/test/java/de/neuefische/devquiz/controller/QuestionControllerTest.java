55f676441484d0fae7048efe40735bb0e4060f42
Anke <a.stein@posteo.de>
2021-10-21 12:01:00 +0200

    Fix tests for QuestionController and PlayController by adding Auth Token to requests

--- backend/src/test/java/de/neuefische/devquiz/controller/QuestionControllerTest.java
+++ backend/src/test/java/de/neuefische/devquiz/controller/QuestionControllerTest.java
@@ -2,14 +2,17 @@ package de.neuefische.devquiz.controller;
 
 import de.neuefische.devquiz.model.Question;
 import de.neuefische.devquiz.repo.QuestionRepo;
+import de.neuefische.devquiz.security.model.AppUser;
+import de.neuefische.devquiz.security.repo.AppUserRepo;
+import de.neuefische.devquiz.security.service.AppUserDetailService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -20,6 +23,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class QuestionControllerTest {
+
+    @Autowired
+    private AppUserRepo userRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -35,13 +44,14 @@ class QuestionControllerTest {
     @Test
     @DisplayName("Should return a list with all questions from db")
     void testListQuestion() {
-        // GIVEN
+        HttpHeaders headers = getHttpHeadersWithJWT();
 
+        // GIVEN
         questionRepo.save(new Question("1", "Question with ID '1'", List.of()));
         questionRepo.save(new Question("2", "Question with ID '2'", List.of()));
         questionRepo.save(new Question("3", "Question with ID '3'", List.of()));
         // WHEN
-        ResponseEntity<Question[]> responseEntity = testRestTemplate.getForEntity("/api/question", Question[].class);
+        ResponseEntity<Question[]> responseEntity = testRestTemplate.exchange("/api/question",  HttpMethod.GET, new HttpEntity<>(headers), Question[].class);
         // THEN
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
         assertThat(responseEntity.getBody(), arrayContainingInAnyOrder(
@@ -55,12 +65,14 @@ class QuestionControllerTest {
     @Test
     @DisplayName("Should return a question object with the given id")
     void testGet() {
+        HttpHeaders headers = getHttpHeadersWithJWT();
+
         // GIVEN
         Question question = new Question("302", "Question with ID '302'", List.of());
 
         questionRepo.save(question);
         // WHEN
-        ResponseEntity<Question> responseEntity = testRestTemplate.getForEntity("/api/question/" + question.getId(), Question.class);
+        ResponseEntity<Question> responseEntity = testRestTemplate.exchange("/api/question/" + question.getId(), HttpMethod.GET, new HttpEntity<>(headers), Question.class);
         // THEN
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
         assertThat(responseEntity.getBody(), is(new Question("302", "Question with ID '302'", List.of())));
@@ -70,12 +82,13 @@ class QuestionControllerTest {
     @Test
     @DisplayName("Should add a new question item to the db")
     void testAddQuestion() {
+        HttpHeaders headers = getHttpHeadersWithJWT();
+
         // GIVEN
         Question questionToAdd = new Question("22", "This is a question", List.of());
 
-
         // WHEN
-        ResponseEntity<Question> postResponseEntity = testRestTemplate.postForEntity("/api/question/", questionToAdd, Question.class);
+        ResponseEntity<Question> postResponseEntity = testRestTemplate.exchange("/api/question/", HttpMethod.POST, new HttpEntity<>(questionToAdd, headers), Question.class);
         Question actual = postResponseEntity.getBody();
 
         // THEN
@@ -84,12 +97,24 @@ class QuestionControllerTest {
         assertThat(actual, is(new Question("22", "This is a question", List.of())));
 
         // THEN - check via GET
-        ResponseEntity<Question> getResponse = testRestTemplate.getForEntity("/api/question/" + questionToAdd.getId(), Question.class);
+        ResponseEntity<Question> getResponse = testRestTemplate.exchange("/api/question/" + questionToAdd.getId(), HttpMethod.GET, new HttpEntity<>(headers), Question.class);
         Question persistedQuestion = getResponse.getBody();
 
         assertNotNull(persistedQuestion);
         assertThat(persistedQuestion.getId(), is(questionToAdd.getId()));
         assertThat(persistedQuestion.getQuestionText(), is(questionToAdd.getQuestionText()));
 
+    }
+
+    private HttpHeaders getHttpHeadersWithJWT() {
+        userRepo.save(AppUser.builder()
+                .username("test-username")
+                .password(passwordEncoder.encode("test-password"))
+                .build());
+        AppUser loginData = new AppUser("test-username", "test-password");
+        ResponseEntity<String> response = testRestTemplate.postForEntity("/auth/login", loginData, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(response.getBody());
+        return(headers);
     }
 }
