534145c09df0312fc5a240fe373baa6c3cc109fa
Anke <a.stein@posteo.de>
2021-10-26 11:42:07 +0200

    Add button for Login with GitHub

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,8 @@
 import {useContext, useState} from "react";
 import styled from "styled-components/macro";
 import {AuthContext} from "../context/AuthProvider";
+import {useHistory} from "react-router-dom";
+import {sendClientId} from "../service/devQuizApiService";
 
 const initialCredentials = {
     username: "",
@@ -11,9 +13,10 @@ export default function LoginPage() {
 
     const [credentials, setCredentials] = useState(initialCredentials)
     const {login} = useContext(AuthContext)
+    const history = useHistory();
 
     const handleChange = event => {
-        setCredentials({...credentials, [event.target.name] : event.target.value})
+        setCredentials({...credentials, [event.target.name]: event.target.value})
     }
 
     const handleSubmit = event => {
@@ -21,29 +24,41 @@ export default function LoginPage() {
         login(credentials)
     }
 
+    const clientId = "20bd6360403e3e1711a8"
+
+    const handleClick = () => {
+        window.open("https://github.com/login/oauth/authorize?client_id=" + clientId)
+    }
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <label>Username
-                <input type="text"
-                       value={credentials.username}
-                       placeholder="Enter username"
-                       required="required"
-                       name="username"
-                       onChange={handleChange}/>
-            </label>
+        <>
+            <Form onSubmit={handleSubmit}>
+                <label>Username
+                    <input type="text"
+                           value={credentials.username}
+                           placeholder="Enter username"
+                           required="required"
+                           name="username"
+                           onChange={handleChange}/>
+                </label>
 
-            <label>Password
-                <input type="password"
-                       value={credentials.password}
-                       placeholder="Enter password"
-                       required="required"
-                       name="password"
-                       onChange={handleChange}/>
-            </label>
+                <label>Password
+                    <input type="password"
+                           value={credentials.password}
+                           placeholder="Enter password"
+                           required="required"
+                           name="password"
+                           onChange={handleChange}/>
+                </label>
 
-            <button>Sign in</button>
-        </Form>
+                <button>Sign in</button>
+            </Form>
+
+            <button onClick={handleClick}>Log in with GitHub</button>
+
+        </>
+
+
     )
 
 
